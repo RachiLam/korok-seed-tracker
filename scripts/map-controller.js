8a7f9045d1535d6421e54173990d30a2c734855d
Rachid Lamouri <rachidlamouri@gmail.com>
2018-09-27 20:18:54 -0400

    Scroll map with wasd

--- scripts/map-controller.js
+++ scripts/map-controller.js
@@ -1,7 +1,29 @@
 // requires state
 
 const mapController = (() => {
+    const mapContainer = $('div.map')
     const [img] = $('div.map img')
+    
+    const scrollAmount = 30
+    $(document.body).keydown(event => {
+        const top = mapContainer.scrollTop()
+        const left = mapContainer.scrollLeft()
+        
+        switch(event.key.toLowerCase()){
+            case 'a':
+                mapContainer.scrollLeft(left - scrollAmount)
+                break
+            case 'd':
+                mapContainer.scrollLeft(left + scrollAmount)
+                break
+            case 'w':
+                mapContainer.scrollTop(top - scrollAmount)
+                break
+            case 's':
+                mapContainer.scrollTop(top + scrollAmount)
+                break
+        }
+    })
     
     return {
         setRegion({filepath, width, height}, zoom){
